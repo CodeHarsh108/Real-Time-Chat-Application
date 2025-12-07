3587cde987a6b2ebd174294caab3f8a108a88587
Harsh Patil <itsharshhh6@gmail.com>
2025-12-07 18:37:08 +0530

    Chat Controller

--- src/main/java/com/harsh/chat/controllers/ChatController.java
+++ src/main/java/com/harsh/chat/controllers/ChatController.java
@@ -1,4 +1,47 @@
 package com.harsh.chat.controllers;
 
+import com.harsh.chat.entity.Message;
+import com.harsh.chat.entity.Room;
+import com.harsh.chat.payload.MessageRequest;
+import com.harsh.chat.repositories.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+
+@Controller
 public class ChatController {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
+    public ChatController(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    // for sending and receiving messages
+    @MessageMapping("/sendMessage/{roomId}")
+    @SendTo("/topic/room/{roomId}")
+    public Message sendMessage(
+            @DestinationVariable String roomId,
+            @RequestBody MessageRequest messageRequest
+    ) {
+        Room room = roomRepository.findByRoomId(roomId);
+        Message message = new Message();
+        message.setContent(messageRequest.getContent());
+        message.setSender(messageRequest.getSender());
+        message.setTimeStamp(LocalDateTime.now());
+
+        if(room != null){
+            room.getMessages().add(message);
+            roomRepository.save(room);
+        }else{
+            throw new RuntimeException("Room with {roomId} not found!!!");
+        }
+        return message;
+    }
 }
