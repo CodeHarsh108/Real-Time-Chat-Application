1dc70caba1b32175abc7cf5939c8ae1be5ffba07
Harsh Patil <itsharshhh6@gmail.com>
2026-01-09 11:33:17 +0530

    Backend Tweaking

--- src/main/java/com/harsh/chat/entity/Message.java
+++ src/main/java/com/harsh/chat/entity/Message.java
@@ -1,9 +1,6 @@
 package com.harsh.chat.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
