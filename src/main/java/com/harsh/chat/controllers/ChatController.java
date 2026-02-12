4f58332451129a6610864bd4bb61adb476f0c488
Harsh Patil <itsharshhh6@gmail.com>
2026-02-12 15:05:51 +0530

    Separated Business Logic from controller

--- src/main/java/com/harsh/chat/controllers/ChatController.java
+++ src/main/java/com/harsh/chat/controllers/ChatController.java
@@ -6,47 +6,43 @@ import com.harsh.chat.entity.Room;
 import com.harsh.chat.payload.MessageRequest;
 import com.harsh.chat.repositories.MessageRepository;
 import com.harsh.chat.repositories.RoomRepository;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import com.harsh.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.*;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 public class ChatController {
 
 
-    private RoomRepository roomRepository;
-
-    private MessageRepository messageRepository;
-
-    public ChatController(RoomRepository roomRepository, MessageRepository messageRepository) {
-        this.roomRepository = roomRepository;
-        this.messageRepository = messageRepository;
-    }
-
+    private final ChatService chatService;
 
     @MessageMapping("/sendMessage/{roomId}")
     @SendTo("/topic/room/{roomId}")
     public Message sendMessage(
             @DestinationVariable String roomId,
-            @RequestBody MessageRequest request
+            @Payload MessageRequest request
     ) {
+        log.info("Received message for room {}: {}", roomId, request);
 
-        Room room = roomRepository.findByRoomId(request.getRoomId())
-                .orElseThrow(() -> new RuntimeException("Room not found!!!"));
-        Message message = Message.create(
-                request.getRoomId(),
-                request.getSender(),
-                request.getContent()
-        );
-        message = messageRepository.save(message);
-        room.addMessage(message.getId());
-        roomRepository.save(room);
-        return message;
+        if (!roomId.equals(request.getRoomId())) {
+            throw new IllegalArgumentException("Room ID in path and body do not match");
+        }
 
-
+        return chatService.saveMessage(request);
     }
+
+    @MessageExceptionHandler
+    @SendToUser("/queue/errors")
+    public String handleException(Exception exception) {
+        log.error("WebSocket error: {}", exception.getMessage());
+        return "Error: " + exception.getMessage();
+    }
+
 }
