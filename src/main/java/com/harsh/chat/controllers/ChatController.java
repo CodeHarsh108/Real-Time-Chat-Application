e500953a8f0574eb74bd3550688b4531f6f46c9b
Harsh Patil <itsharshhh6@gmail.com>
2026-02-12 10:59:08 +0530

    Chat Endpoints Improved

--- src/main/java/com/harsh/chat/controllers/ChatController.java
+++ src/main/java/com/harsh/chat/controllers/ChatController.java
@@ -4,6 +4,7 @@ package com.harsh.chat.controllers;
 import com.harsh.chat.entity.Message;
 import com.harsh.chat.entity.Room;
 import com.harsh.chat.payload.MessageRequest;
+import com.harsh.chat.repositories.MessageRepository;
 import com.harsh.chat.repositories.RoomRepository;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 
 @RestController
 public class ChatController {
@@ -20,31 +20,31 @@ public class ChatController {
 
     private RoomRepository roomRepository;
 
-    public ChatController(RoomRepository roomRepository) {
+    private MessageRepository messageRepository;
+
+    public ChatController(RoomRepository roomRepository, MessageRepository messageRepository) {
         this.roomRepository = roomRepository;
+        this.messageRepository = messageRepository;
     }
 
 
-    //for sending and receiving messages
-    @MessageMapping("/sendMessage/{roomId}")// /app/sendMessage/roomId
-    @SendTo("/topic/room/{roomId}")//subscribe
+    @MessageMapping("/sendMessage/{roomId}")
+    @SendTo("/topic/room/{roomId}")
     public Message sendMessage(
             @DestinationVariable String roomId,
             @RequestBody MessageRequest request
     ) {
 
-        Room room = roomRepository.findByRoomId(request.getRoomId());
-        Message message = new Message();
-        message.setContent(request.getContent());
-        message.setSender(request.getSender());
-        message.setTimeStamp(LocalDateTime.now());
-        if (room != null) {
-            room.getMessages().add(message);
-            roomRepository.save(room);
-        } else {
-            throw new RuntimeException("room not found !!");
-        }
-
+        Room room = roomRepository.findByRoomId(request.getRoomId())
+                .orElseThrow(() -> new RuntimeException("Room not found!!!"));
+        Message message = Message.create(
+                request.getRoomId(),
+                request.getSender(),
+                request.getContent()
+        );
+        message = messageRepository.save(message);
+        room.addMessage(message.getId());
+        roomRepository.save(room);
         return message;
 
 
