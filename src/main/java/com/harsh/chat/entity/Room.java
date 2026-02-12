d7696752d5115d4749e80ef5b8637cdea4440547
Harsh Patil <itsharshhh6@gmail.com>
2026-02-12 10:58:44 +0530

    Room Entity Improved

--- src/main/java/com/harsh/chat/entity/Room.java
+++ src/main/java/com/harsh/chat/entity/Room.java
@@ -1,12 +1,13 @@
 package com.harsh.chat.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +16,37 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Room {
     @Id
     private String id;//Mongo db : unique identifier
-    private String roomId;
-    private List<Message> messages = new ArrayList<>();
 
+    @Indexed(unique = true)
+    private String roomId;
+
+    @Builder.Default
+    private List<String> recentMessageIds = new ArrayList<>();
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @Builder.Default
+    private Integer totalMessages = 0;
+
+    public void addMessage(String messageId){
+        this.recentMessageIds.add(0, messageId);
+        if(this.recentMessageIds.size() > 50){
+            this.recentMessageIds = this.recentMessageIds.subList(0, 50);
+        }
+        this.totalMessages++;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public boolean isNew(){
+        return this.createdAt == null;
+    }
 
 }
