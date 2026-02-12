0f7ad5ece00121cde5c68f23a9b4b61ca4c4b7d5
Harsh Patil <itsharshhh6@gmail.com>
2026-02-12 15:06:32 +0530

    Updated Repo

--- src/main/java/com/harsh/chat/repositories/RoomRepository.java
+++ src/main/java/com/harsh/chat/repositories/RoomRepository.java
@@ -2,9 +2,23 @@ package com.harsh.chat.repositories;
 
 
 import com.harsh.chat.entity.Room;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 public interface RoomRepository extends MongoRepository<Room, String> {
-    //get room using room id
-    Room findByRoomId(String roomId);
+
+    Optional<Room> findByRoomId(String roomId);
+
+    boolean existsByRoomId(String roomId);
+
+    void deleteByRoomId(String roomId);
+
+
 }
