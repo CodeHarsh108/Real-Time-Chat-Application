12d75a5616f0d09ecef3df2b56f5e2338a6221ba
Harsh Patil <itsharshhh6@gmail.com>
2026-02-12 15:06:45 +0530

    Updated Repo

---
+++ src/main/java/com/harsh/chat/repositories/MessageRepository.java
@@ -0,0 +1,24 @@
+package com.harsh.chat.repositories;
+
+import com.harsh.chat.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends MongoRepository<Message, String> {
+
+    Page<Message> findByRoomIdOrderByTimestampDesc(String roomId, Pageable pageable);
+
+    List<Message> findTop50ByRoomIdOrderByTimestampDesc(String roomId);
+
+    long countByRoomId(String roomId);
+
+    void deleteByRoomId(String roomId);
+
+}
