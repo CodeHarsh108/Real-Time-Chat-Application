37f3c2364ac396bf1797b636abe446fe26dc4254
Harsh Patil <itsharshhh6@gmail.com>
2026-02-15 12:14:40 +0530

    Custom Annotation : Rate Limit

---
+++ src/main/java/com/harsh/chat/annotation/RateLimit.java
@@ -0,0 +1,14 @@
+package com.harsh.chat.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RateLimit {
+    int maxAttempts() default 10;
+    int timeWindow() default 60;
+    String key() default "";
+}
